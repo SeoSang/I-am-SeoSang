7a13d2628e4fb2c6a28924306a93275081322bf5
서상혁 <ddrrpg@naver.com>
2023-01-10 22:01:56 +0900

    프로필 페이지 대폭 개선

--- components/profile/AboutMe.tsx
+++ components/profile/AboutMe.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { INDEX_LINE_COLOR, H3_KR, A } from "../../styles/styled";
-import { Row, Col } from "antd";
+import {H3_KR, INDEX_LINE_COLOR} from "../../styles/styled";
+import {Col, Row} from "antd";
 import styled from "styled-components";
 
 const SummaryH3 = styled(H3_KR)`
@@ -10,112 +10,112 @@ const SummaryH3 = styled(H3_KR)`
 `;
 
 const AboutMe = () => {
-  return (
-    <Row style={{ margin: "20px 10px" }}>
-      <Col xs={24} md={6}>
-        <img src="/profile/profile_seo.jpg" style={{ maxWidth: "100%" }}></img>
-      </Col>
-      <Col xs={24} md={12} style={{ padding: "0 5vw" }}>
-        <SummaryH3>⭐요약⭐</SummaryH3>
-        <ul>
-          <li> 1997.06.09 ~ 서울 망원동 </li>
-          <li> 고려대학교 컴퓨터학과 졸업 </li>
-          <li>
-            {" "}
-            관심분야 : 웹(코딩)📟, 컴퓨터 주변기기💻, 축구⚽, 게임🎮, 강아지🐶
-          </li>
-          <li>
-            {" "}
-            장점 : 한번 시작했으면 끝을 본다 | 배움을 즐긴다 | 적응력이 좋다
-          </li>
-          <li>
-            {" "}
-            단점 : 꼼꼼함의 부족 | 기억력이 안좋아 메모습관을 강제로 주입중😣
-          </li>
-          <br></br>
-          <li> 2015 고려대학교 컴퓨터학과 입학</li>
-          <li> 2016.03 ~ 2018.03 공군 암호체계병 병장 만기전역</li>
-          <li> 2017 ~ 2018 제 10전투비행단 ICT 프로그래밍 동아리</li>
-          <li>
-            {" "}
-            2019 하나와영 해커톤 최우수상 수상 :{" "}
-            <a
-              style={{ color: INDEX_LINE_COLOR }}
-              href="https://github.com/beygee/minki"
-              target="_blank"
-            >
-              <strong>Minki</strong>
-            </a>
-          </li>
-          <li>
-            {" "}
-            2019 ICT멘토링 공모전 본선 입선 :{" "}
-            <a
-              style={{ color: INDEX_LINE_COLOR }}
-              href="https://www.youtube.com/watch?v=DTgJO6ti7s4"
-              target="_blank"
-            >
-              <strong>Berry IP</strong>
-            </a>
-          </li>
-          <li>
-            2019 블로그 개설 :{" "}
-            <a
-              style={{ color: INDEX_LINE_COLOR }}
-              href="https://programming119.tistory.com"
-              target="_blank"
-            >
-              <strong>개발자 아저씨들 힘을 모아</strong>
-            </a>
-          </li>
-          <li>
-            2020 독학의 해, 잡다한 토이 프로젝트들 진행중 /
-            <a
-              style={{ color: INDEX_LINE_COLOR }}
-              href="https://github.com/SeoSang"
-              target="_blank"
-            >
-              <strong>Github</strong>
-            </a>
-          </li>
-          <li>
-            {" "}
-            2020 하절기 삼성 SDS 대학생 알고리즘 특강 Professional 과정 수료 /{" "}
-            <a
-              style={{ color: INDEX_LINE_COLOR }}
-              href="https://programming119.tistory.com/175"
-              target="_blank"
-            >
-              <strong>후기</strong>
-            </a>
-          </li>
-          <li> 2020 삼성 SDS 알고리즘 Professional 등급 취득 / </li>
-        </ul>
-      </Col>
-      <Col xs={24} md={6}>
-        <H3_KR>📑 경력 </H3_KR>
-        <ul>
-          <li> 2020.12.28 ~ 2020.02.27 카카오 추천팀 인턴 💛 </li>
-          <li> 2021.03.15 ~ 2021.05.14 네이버 쇼핑검색서비스 인턴 💚 </li>
-          <li> 2021.07.26 ~ 네이버 쇼핑검색서비스 FE 개발자 🍀 </li>
-        </ul>
-        <br></br>
-        <H3_KR>🧾 자격증 </H3_KR>
-        <ul>
-          <li> 삼성 SDS 알고리즘 Professional 등급 </li>
-          <li> 컴활 1급</li>
-          <li> 워드프로세서 1급 </li>
-          <li> 정보처리 기능사 1급 </li>
-        </ul>
-        <br></br>
-        <H3_KR>😎 기타</H3_KR>
-        <ul>
-          <li> 프로그래머스 점수랭킹 83위 </li>
-          <li> 백준 플레티넘 4 (21.04.18 기준)</li>
-        </ul>
-      </Col>
-    </Row>
-  );
+    return (
+        <Row style={{margin: "20px 10px"}}>
+            <Col xs={24} md={6}>
+                <img src="/profile/profile_seo.jpg" style={{maxWidth: "100%"}}></img>
+            </Col>
+            <Col xs={24} md={12} style={{padding: "0 5vw"}}>
+                <SummaryH3>⭐요약⭐</SummaryH3>
+                <ul>
+                    <li> 1997.06.09 ~ 서울 망원동</li>
+                    <li> 고려대학교 컴퓨터학과 졸업</li>
+                    <li>
+                        {" "}
+                        관심분야 : 코딩📟, 컴퓨터 주변기기💻, 운동💪🏼, 축구⚽, 게임🎮, 강아지🐶
+                    </li>
+                    <li>
+                        {" "}
+                        장점 : 한번 시작했으면 끝을 본다 | 배움을 즐긴다 | 적응력이 좋다
+                    </li>
+                    <li>
+                        {" "}
+                        단점 : 꼼꼼함의 부족 | 기억력이 안좋아 메모습관을 강제로 주입중😣
+                    </li>
+                    <br></br>
+                    <li> 2015 고려대학교 컴퓨터학과 입학</li>
+                    <li> 2016.03 ~ 2018.03 공군 암호체계병 병장 만기전역</li>
+                    <li> 2017 ~ 2018 제 10전투비행단 ICT 프로그래밍 동아리</li>
+                    <li>
+                        {" "}
+                        2019 하나와영 해커톤 최우수상 수상 :{" "}
+                        <a
+                            style={{color: INDEX_LINE_COLOR}}
+                            href="https://github.com/beygee/minki"
+                            target="_blank"
+                        >
+                            <strong>Minki</strong>
+                        </a>
+                    </li>
+                    <li>
+                        {" "}
+                        2019 ICT멘토링 공모전 본선 입선 :{" "}
+                        <a
+                            style={{color: INDEX_LINE_COLOR}}
+                            href="https://www.youtube.com/watch?v=DTgJO6ti7s4"
+                            target="_blank"
+                        >
+                            <strong>Berry IP</strong>
+                        </a>
+                    </li>
+                    <li>
+                        2019 블로그 개설 :{" "}
+                        <a
+                            style={{color: INDEX_LINE_COLOR}}
+                            href="https://programming119.tistory.com"
+                            target="_blank"
+                        >
+                            <strong>개발자 아저씨들 힘을 모아</strong>
+                        </a>
+                    </li>
+                    <li>
+                        2020 독학의 해, 잡다한 토이 프로젝트들 진행중 /
+                        <a
+                            style={{color: INDEX_LINE_COLOR}}
+                            href="https://github.com/SeoSang"
+                            target="_blank"
+                        >
+                            <strong>Github</strong>
+                        </a>
+                    </li>
+                    <li>
+                        {" "}
+                        2020 하절기 삼성 SDS 대학생 알고리즘 특강 Professional 과정 수료 /{" "}
+                        <a
+                            style={{color: INDEX_LINE_COLOR}}
+                            href="https://programming119.tistory.com/175"
+                            target="_blank"
+                        >
+                            <strong>후기</strong>
+                        </a>
+                    </li>
+                    <li> 2020 삼성 SDS 알고리즘 Professional 등급 취득 /</li>
+                </ul>
+            </Col>
+            <Col xs={24} md={6}>
+                <H3_KR>📑 경력 </H3_KR>
+                <ul>
+                    <li> 2020.12.28 ~ 2020.02.27 카카오 추천팀 인턴 💛</li>
+                    <li> 2021.03.15 ~ 2021.05.14 네이버 쇼핑검색서비스 인턴 💚</li>
+                    <li> 2021.07.26 ~ 네이버 쇼핑검색서비스 FE 개발자 🍀</li>
+                </ul>
+                <br></br>
+                <H3_KR>🧾 자격증 </H3_KR>
+                <ul>
+                    <li> 삼성 SDS 알고리즘 Professional 등급</li>
+                    <li> 컴활 1급</li>
+                    <li> 워드프로세서 1급</li>
+                    <li> 정보처리 기능사 1급</li>
+                </ul>
+                <br></br>
+                {/*<H3_KR>😎 기타</H3_KR>*/}
+                {/*<ul>*/}
+                {/*  <li> 프로그래머스 점수랭킹 83위 </li>*/}
+                {/*  <li> 백준 플레티넘 4 (21.04.18 기준)</li>*/}
+                {/*</ul>*/}
+            </Col>
+        </Row>
+    );
 };
 
 export default AboutMe;
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,15 +1,17 @@
-import React from "react"
+import React, {useState} from "react"
 import styled from "styled-components"
 import AboutMe from "../components/profile/AboutMe"
 import TechUsed from "../components/profile/TechUsed"
-import { Divider, Row, Col, BackTop } from "antd"
+import {BackTop, Divider} from "antd"
 import {
-  SmileOutlined,
-  RadarChartOutlined,
-  PictureOutlined,
-  FundProjectionScreenOutlined,
+    DownOutlined,
+    FundProjectionScreenOutlined,
+    PictureOutlined,
+    RadarChartOutlined,
+    SmileOutlined,
+    UpOutlined
 } from "@ant-design/icons"
-import { INDEX_BG_COLOR, INDEX_TEXT_COLOR } from "../styles/styled"
+import {INDEX_BG_COLOR, INDEX_TEXT_COLOR} from "../styles/styled"
 import Photo from "../components/profile/Photo"
 import Project from "../components/profile/Project"
 
@@ -32,29 +34,45 @@ const BackTopDiv = styled.div`
 `
 
 const profile = () => {
-  return (
-    <ProfileContainer>
-      <Divider style={{ margin: "40px 0" }} orientation='left'>
-        <SmileOutlined /> About me
-      </Divider>
-      <AboutMe></AboutMe>
-      <Divider style={{ margin: "40px 0" }} orientation='center'>
-        <RadarChartOutlined /> Tech Used
-      </Divider>
-      <TechUsed></TechUsed>
-      <Divider style={{ margin: "40px 0" }} orientation='center'>
-        <FundProjectionScreenOutlined /> {"  Toy Projects"}
-      </Divider>
-      <Project></Project>
-      <Divider style={{ margin: "80px 0" }} orientation='center'>
-        <PictureOutlined /> Photo
-      </Divider>
-      <Photo></Photo>
-      <BackTop>
-        <BackTopDiv>UP</BackTopDiv>
-      </BackTop>
-    </ProfileContainer>
-  )
+    const [techUsedActive, setTechUsedActive] = useState(false)
+    const [projectActive, setProjectActive] = useState(false)
+    const [photoActive, setPhotoActive] = useState(false)
+
+    return (
+        <ProfileContainer>
+            <Divider style={{margin: "40px 0"}} orientation='left'>
+                <SmileOutlined/> About me
+            </Divider>
+            <AboutMe/>
+            <Divider style={{margin: "40px 0"}} orientation='center'>
+                <RadarChartOutlined/> Tech Used
+            </Divider>
+            <div style={{cursor: 'pointer'}} onClick={(e) => setTechUsedActive(prev => !prev)}>
+                <span>{techUsedActive ? '접기' : '자세히보기'} </span>
+                {techUsedActive ? <UpOutlined/> : <DownOutlined/>}
+            </div>
+            {techUsedActive && <TechUsed/>}
+            <Divider style={{margin: "40px 0"}} orientation='center'>
+                <FundProjectionScreenOutlined/> {"  Toy Projects"}
+            </Divider>
+            <div style={{cursor: 'pointer'}} onClick={(e) => setProjectActive(prev => !prev)}>
+                <span>{projectActive ? '접기' : '자세히보기'} </span>
+                {projectActive ? <UpOutlined/> : <DownOutlined/>}
+            </div>
+            {projectActive && <Project/>}
+            <Divider style={{margin: "40px 0 40px 0"}} orientation='center'>
+                <PictureOutlined/> Photo
+            </Divider>
+            <div style={{cursor: 'pointer'}} onClick={(e) => setPhotoActive(prev => !prev)}>
+                <span>{photoActive ? '접기' : '자세히보기'} </span>
+                {photoActive ? <UpOutlined/> : <DownOutlined/>}
+            </div>
+            {photoActive && <Photo/>}
+            <BackTop>
+                <BackTopDiv>UP</BackTopDiv>
+            </BackTop>
+        </ProfileContainer>
+    )
 }
 
 export default profile
