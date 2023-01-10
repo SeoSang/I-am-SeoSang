f3feb7ed74f8bdcb87138947884d0386a7b45ab2
서상혁 <ddrrpg@naver.com>
2023-01-10 22:20:04 +0900

    photo에 jotai 발표 추가

--- components/profile/Photo.tsx
+++ components/profile/Photo.tsx
@@ -1,6 +1,6 @@
 import React from "react"
-import { FlexDiv, INDEX_TEXT_COLOR, H2, A } from "../../styles/styled"
-import { Card } from "antd"
+import {A, FlexDiv, H2, INDEX_TEXT_COLOR} from "../../styles/styled"
+import {Card} from "antd"
 import Meta from "antd/lib/card/Meta"
 import styled from "styled-components"
 
@@ -30,36 +30,39 @@ const CardMeta = styled(Meta)`
 `
 
 const Photo = () => {
-  return (
-    <FlexDiv direction='column'>
-      <H2 color={INDEX_TEXT_COLOR}>Photos </H2>
-      <FlexDiv style={{ flexWrap: "wrap", justifyContent: "space-around" }}>
-        <PhotoCard cover={<img src='/profile/photos/2019_hackaton.jpg' />}>
-          <CardMeta
-            title='2019 하나와영 해커톤'
-            description={
-              <p>
-                CMD 속의 고양이 Minki !<br></br>
-                <A href='https://github.com/beygee/minki'>깃허브</A>
-              </p>
-            }
-          />
-        </PhotoCard>
-        <PhotoCard cover={<img src='/profile/photos/me_hackaton.jpg' />}>
-          <CardMeta title='2020 고려대학교 해커톤' description='Googri!' />
-        </PhotoCard>
-        <PhotoCard cover={<img src='/profile/photos/sds.jpg' />}>
-          <CardMeta
-            title='2020 삼성 SDS 알고리즘 특강 수료증'
-            description='다들 수고 많으셨습니다!'
-          />
-        </PhotoCard>
-        <PhotoCard cover={<img src='/profile/photos/sds_professional.png' />}>
-          <CardMeta title='SDS Professional' description='합격!!!🔥🔥🔥' />
-        </PhotoCard>
-      </FlexDiv>
-    </FlexDiv>
-  )
+    return (
+        <FlexDiv direction='column'>
+            <H2 color={INDEX_TEXT_COLOR}>Photos </H2>
+            <FlexDiv style={{flexWrap: "wrap", justifyContent: "space-around"}}>
+                <PhotoCard cover={<img src='/profile/photos/2019_hackaton.jpg'/>}>
+                    <CardMeta
+                        title='2019 하나와영 해커톤'
+                        description={
+                            <p>
+                                CMD 속의 고양이 Minki !<br></br>
+                                <A href='https://github.com/beygee/minki'>깃허브</A>
+                            </p>
+                        }
+                    />
+                </PhotoCard>
+                <PhotoCard cover={<img src='/profile/photos/me_hackaton.jpg'/>}>
+                    <CardMeta title='2020 고려대학교 해커톤' description='Googri!'/>
+                </PhotoCard>
+                <PhotoCard cover={<img src='/profile/photos/sds.jpg'/>}>
+                    <CardMeta
+                        title='2020 삼성 SDS 알고리즘 특강 수료증'
+                        description='다들 수고 많으셨습니다!'
+                    />
+                </PhotoCard>
+                <PhotoCard cover={<img src='/profile/photos/sds_professional.png'/>}>
+                    <CardMeta title='SDS Professional' description='합격 🔥🔥🔥'/>
+                </PhotoCard>
+                <PhotoCard cover={<img src='/profile/photos/jotai.jpeg'/>}>
+                    <CardMeta title='2022 FE 라운지 Jotai 발표' description='https://jotai.org/'/>
+                </PhotoCard>
+            </FlexDiv>
+        </FlexDiv>
+    )
 }
 
 export default Photo
